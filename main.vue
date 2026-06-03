e4a9255a1eb82f9e7cd85214ba3cc916453637cd
Thomas Becannon <tbecannon13@gmail.com>
2026-06-02 20:48:02 -0700

    Blank

--- main.vue
+++ main.vue
@@ -1,19 +1,3 @@
 <template>
-    <head>
-        <title>Blue Collar Software</title>
-    </head>
-    <body>
-
-        <header>
-            <h1>Blue Collar Software</h1>
-        </header>
-
-        <main>
-            <p>This is where your main webpage content goes.</p>
-        </main>
-
-        <footer>
-            <p>&copy; 2026 My Website</p>
-        </footer>
-    </body>
+  <div></div>
 </template>
